ae884f9f3e6b3389b5cfb0053370da36fa155722
Cxiong29 <145512375+Cxiong29@users.noreply.github.com>
2025-02-09 21:55:53 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+using System.Text;
+using w2_EnhanncedFileIO.Services;
+
+namespace w1_in_class
+{
+    public class Program
+
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1. Display Characters");
+            Console.WriteLine("2. Add Character");
+            Console.WriteLine("3. Level Up Character");
+            Console.WriteLine("-----------------------");
+
+
+            var menuOption = Console.ReadLine();
+
+            if (menuOption == "1")
+            {
+               CharacterManager.DisplayCharacters();
+
+            }
+
+            if (menuOption == "2")
+            {
+                CharacterManager.AddCharacter();
+
+            }
+
+            if (menuOption == "3")
+            {
+                CharacterManager.LevelUpCharacter();
+
+            }
+
+
+
+        }
+       
+
+    }
+  
+}
